6ae434d35fcfc4c6ae918bab805b5f15f199efcc
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2019-05-21 12:06:44 +0530

    Tests are passing.

--- src/test/java/com/example/todo/TodoControllerTest.java
+++ src/test/java/com/example/todo/TodoControllerTest.java
@@ -1,6 +1,6 @@
 package com.example.todo;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
